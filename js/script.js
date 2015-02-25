adaf1d5aa296f771367ea6d41e6c414152d9cf55
Yizhu Wang <abamboo319@gmail.com>
2015-02-24 19:52:39 -0600

    2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
+    width = $(".chart").width() - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var parseDate = d3.time.format("%d-%b-%y").parse;
+var parseDate = d3.time.format("%d-%b-%y").parse;  //a time format of d3//
 
 var x = d3.time.scale()
     .range([0, width]);
@@ -16,26 +17,33 @@ var xAxis = d3.svg.axis()
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left");
+    .orient("left")
+  .tickFormat(function(d) {
+    	return d.toFixed(1) + "%";
+     }); 
 
 var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+    .y(function(d) { return y(d.unemployment_rate); });
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function(error, data) {
+d3.json("js/data.json", function(error, data) {
+	console.log(data);
+
   data.forEach(function(d) {
     d.date = parseDate(d.date);
-    d.close = +d.close;
+    d.unemployment_rate = +d.unemployment_rate;   //turing our string version of time into a formate that d3 can read//
   });
 
   x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.close; }));
+  y.domain(d3.extent(data, function(d) { return d.unemployment_rate; })); //time dates and space//
+  //([0,20])
+  
 
   svg.append("g")
       .attr("class", "x axis")
@@ -50,12 +58,40 @@ d3.tsv("data.tsv", function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Price ($)");
+      .text("Unemployment Rate (%)");
 
   svg.append("path")
       .datum(data)
       .attr("class", "line")
       .attr("d", line);
+
+  svg.selectAll(".dot")
+  .data(data)
+  .enter()
+  .append("circle")
+  .attr("cx", function(d){
+  	return x(d.date);
+  })
+
+.attr("cy", function(d){
+  	return y(d.unemployment_rate);
+  })
+
+.attr("r", 5)
+.on("mouseover",function(d){
+	var dispDate = moment(d.date).format("MMM.D, YYYY");
+	$(".tt").html(
+"<div class='date'>"+dispDate+"</div>"+
+"<div class='val'>"+d.unemployment_rate+"</div>"
+
+		);
+})
+
+
+
 });
+
+
+
 
 
